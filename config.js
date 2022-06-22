b4177c0113fbabd47935677a8ae88c252f3e1d0c
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2022-06-22 14:40:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ global.nameowner = 'Vinnstore'
 global.numberowner = '6283185091475'
 global.instagram = 'https://instagram.com/ikykunnnn'
 global.github = 'https://github.com/rizkiwibu'
-global.dana = '6283817161861'
-global.pulsa = '6283817161861'
-global.gopay = '6283817161861'
+global.dana = '081366794722'
+global.pulsa = '081366794722'
+global.gopay = '081366794722'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'ikyBot-Md'
+global.namebot = 'VINN BOT'
 global.gc = 'https://chat.whatsapp.com/KcMIbBUddqjIoNmGCrRU5m'
 global.web = 'https://www.helgastoree.xyz' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
@@ -23,10 +23,10 @@ global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti ja
 global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = 'iky Beta'
+global.wm = 'VINN BOT'
 global.watermark = wm
-global.wm2 = '                     「 iky Beta あ⁩ 」'
-global.wm3 = '⫹⫺ iky Beta'
+global.wm2 = '                     「 VINN BOT あ⁩ 」'
+global.wm3 = '⫹⫺ VINN BOT'
 global.htki = '––––––『' 
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
